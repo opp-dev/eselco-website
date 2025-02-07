092e3fa8db7103959b3877898861b2e8168a776f
Suryanshu Rai <140107575+opp-dev@users.noreply.github.com>
2025-02-07 16:13:41 +0100

    Footer link update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         </main>
         <footer className="container mx-auto p-4 text-center text-sm text-gray-500">
         <p>
-          Created using Next.js, React.js, TypeScript, and Tailwind CSS. Repository hosted on <a href="https://github.com/yourusername/yourrepo" target="_blank" rel="noopener noreferrer" className="underline">GitHub</a>. Content managed with <a href="https://obsidian.md/" target="_blank" rel="noopener noreferrer" className="underline">Obsidian</a>.
+          Created using Next.js, React.js, TypeScript, and Tailwind CSS. Repository hosted on <a href="https://github.com/opp-dev/eselco-website" target="_blank" rel="noopener noreferrer" className="underline">GitHub</a>. Content managed with <a href="https://obsidian.md/" target="_blank" rel="noopener noreferrer" className="underline">Obsidian</a>.
         </p>
       </footer>
       <Analytics />
