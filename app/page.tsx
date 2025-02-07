87827ee208b5aaa1f75f80d611c2a8fa048121ba
Suryanshu Rai <140107575+opp-dev@users.noreply.github.com>
2025-02-07 22:23:36 +0100

    styling changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <PageWrapper>
       <div>
-        <h1>Welcome to Eselco</h1>
-          <p>This is the index page.
+        <h1 className='w-fit mx-auto pb-8'>Welcome to Eselco</h1>
+          <p className='max-w-[786px] mx-auto'>This is the index page.
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.   
           </p>
         </div>
