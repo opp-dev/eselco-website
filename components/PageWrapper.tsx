69df6775544c8571cf849ac6b5e72acbf357092a
Suryanshu Rai <140107575+opp-dev@users.noreply.github.com>
2025-02-07 22:03:18 +0100

    Added max width for content to 1280px

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const PageWrapper = ({ children }: Props) => {
   return (
-    <div className="px-4 tablet-v:px-8 tablet-h:px-12 laptop:px-16 desktop:px-20 mx-auto">
+    <div className="px-4 tablet-v:px-8 tablet-h:px-12 laptop:px-16 laptop:max-w-[1280px] mx-auto">
       {children}
     </div>
   );
